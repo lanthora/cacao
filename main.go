8998167bb874cef0a13337499abc9c442a80bfc7
lanthora <lanthora@outlook.com>
2024-07-18 14:43:53 +0800

    feat: add user register api

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/lanthora/cacao/argp"
 	"github.com/lanthora/cacao/logger"
+	"github.com/lanthora/cacao/user"
 )
 
+func init() {
+	gin.SetMode(gin.ReleaseMode)
+}
+
 func main() {
-	listen := argp.Get("listen", ":80")
-	logger.Info("listen=[%v]", listen)
+	addr := argp.Get("listen", ":80")
+	logger.Info("listen=[%v]", addr)
+
+	r := gin.New()
+	r.POST("/api/user/register", user.Register)
+
+	if err := r.Run(addr); err != nil {
+		logger.Fatal("service run failed: %v", err)
+	}
 }
