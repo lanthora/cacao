b4a8dd68bcf008472f88267190e7468529d10bf8
lanthora <lanthora@outlook.com>
2024-07-22 22:11:17 +0800

    feat: add home page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,18 +2,14 @@ import { createWebHistory, createRouter } from 'vue-router'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import LoadingView from './LoadingView.vue'
 import RegisterView from './RegisterView.vue'
 import LoginView from './LoginView.vue'
-import UserView from './UserView.vue'
-import AdminView from './AdminView.vue'
+import HomeView from './HomeView.vue'
 
 const routes = [
-  { path: '/', component: LoadingView },
+  { path: '/', component: HomeView },
   { path: '/register', component: RegisterView },
-  { path: '/login', component: LoginView },
-  { path: '/user', component: UserView },
-  { path: '/admin', component: AdminView }
+  { path: '/login', component: LoginView }
 ]
 
 const router = createRouter({
