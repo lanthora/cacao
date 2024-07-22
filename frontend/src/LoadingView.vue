c11a1b48f3892742896cca7ac3a6021ce6c5706d
lanthora <lanthora@outlook.com>
2024-07-22 16:39:27 +0800

    feat: add loading and login page

---
+++ frontend/src/LoadingView.vue
@@ -0,0 +1,30 @@
+<template>
+  <a-spin />
+</template>
+
+<script setup>
+import axios from 'axios'
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(() => {
+  userInfo()
+})
+
+const userInfo = async () => {
+  const response = await axios.post('/api/user/info')
+  const status = response.data.status
+  if (status == 0) {
+    const role = response.data.data.role
+    if (role == 'admin') {
+      router.push('/admin')
+    } else if (role == 'normal') {
+      router.push('/user')
+    }
+  } else if (status == 2) {
+    router.push('/login')
+  }
+}
+</script>
