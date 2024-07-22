af703d713aca37e2333301f1b6e42a19d2ace316
lanthora <lanthora@outlook.com>
2024-07-22 12:58:37 +0800

    feat: add frontend router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,22 @@
-import './assets/main.css'
-
+import { createWebHistory, createRouter } from 'vue-router'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import RegisterView from './RegisterView.vue'
+import LoginView from './LoginView.vue'
+import UserView from './UserView.vue'
+import AdminView from './AdminView.vue'
+
+const routes = [
+  { path: '/register', component: RegisterView },
+  { path: '/login', component: LoginView },
+  { path: '/user', component: UserView },
+  { path: '/admin', component: AdminView }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+createApp(App).use(router).mount('#app')
