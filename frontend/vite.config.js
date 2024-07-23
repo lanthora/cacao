23ae422269d212a1ba82a1b929febe7dbfaccc9b
lanthora <lanthora@outlook.com>
2024-07-23 11:36:41 +0800

    fix: chunks are too large

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -26,5 +26,16 @@ export default defineConfig({
     proxy: {
       '/api': 'https://canets.org'
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        }
+      }
+    }
   }
 })
