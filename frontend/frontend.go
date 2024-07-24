caf8d2662f65da6f5e2abe3639992dab8dd3c422
lanthora <lanthora@outlook.com>
2024-07-24 12:20:50 +0800

    feat: disable webui default

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -3,10 +3,19 @@ package frontend
 import (
 	"embed"
 	"net/http"
+	"os"
 	"path"
 
 	"github.com/gin-gonic/gin"
 )
+
+var webUiEnabled = false
+
+func init() {
+	if _, ok := os.LookupEnv("CACAOUI"); ok {
+		webUiEnabled = true
+	}
+}
 
 //go:embed dist/*
 var staticFS embed.FS
@@ -20,18 +29,19 @@ var contentType = map[string]string{
 }
 
 func Static(c *gin.Context) {
-	filePath := "dist" + c.Request.URL.String()
-	if data, err := staticFS.ReadFile(filePath); err == nil {
-		c.Header("Cache-Control", "public, max-age=604800")
-		c.Data(http.StatusOK, contentType[path.Ext(filePath)], data)
-		return
-	}
+	if webUiEnabled {
+		filePath := "dist" + c.Request.URL.String()
+		if data, err := staticFS.ReadFile(filePath); err == nil {
+			c.Header("Cache-Control", "public, max-age=604800")
+			c.Data(http.StatusOK, contentType[path.Ext(filePath)], data)
+			return
+		}
 
-	indexPath := "dist/index.html"
-	if data, err := staticFS.ReadFile(indexPath); err == nil {
-		c.Data(http.StatusOK, contentType[path.Ext(indexPath)], data)
-		return
+		indexPath := "dist/index.html"
+		if data, err := staticFS.ReadFile(indexPath); err == nil {
+			c.Data(http.StatusOK, contentType[path.Ext(indexPath)], data)
+			return
+		}
 	}
-
 	c.Status(http.StatusNotFound)
 }
