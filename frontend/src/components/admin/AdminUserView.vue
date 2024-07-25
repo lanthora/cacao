fce920e0714a05eb28b9c4c90edfb43a7263e988
lanthora <lanthora@outlook.com>
2024-07-26 00:26:53 +0800

    fix: admin user view ui

--- frontend/src/components/admin/AdminUserView.vue
+++ frontend/src/components/admin/AdminUserView.vue
@@ -4,7 +4,7 @@
   </a-layout-header>
   <a-layout-content :style="{ margin: '24px 16px 0' }">
     <div :style="{ padding: '24px', background: '#fff' }">
-      <a-form layout="inline" :model="userState">
+      <a-form :model="userState">
         <a-form-item>
           <a-input v-model:value="userState.username" placeholder="Username">
             <template #prefix><UserOutlined style="color: rgba(0, 0, 0, 0.25)" /></template>
@@ -16,22 +16,22 @@
           </a-input>
         </a-form-item>
         <a-form-item>
-          <a-button
-            type="primary"
-            @click="adminAddUser"
-            :disabled="userState.username === '' || userState.password === ''"
-          >
-            Create
-          </a-button>
-        </a-form-item>
-        <a-form-item>
-          <a-button
-            type="primary"
-            @click="adminUpdateUserPassword"
-            :disabled="userState.username === '' || userState.password === ''"
-          >
-            Update
-          </a-button>
+          <a-space>
+            <a-button
+              type="primary"
+              @click="adminAddUser"
+              :disabled="userState.username === '' || userState.password === ''"
+            >
+              Create
+            </a-button>
+            <a-button
+              type="primary"
+              @click="adminUpdateUserPassword"
+              :disabled="userState.username === '' || userState.password === ''"
+            >
+              Update
+            </a-button>
+          </a-space>
         </a-form-item>
       </a-form>
     </div>
